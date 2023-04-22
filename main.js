aa27e229ee4a74cffd7cf2625dd67e00a2c60787
Diego Mancilla <dmancilla19@alumnos.utalca.cl>
2023-04-22 19:23:36 -0400

    prototipo login v1.3 cerrar sesion
    implementación de la función de cerrar sesion luego de registrarse

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js"
-import { getDocs, collection } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js"
-
+//import { getDocs, collection } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js"
+import {auth} from "./app/firebase.js"
+import {loginCheck} from './app/loginCheck.js'
 import './app/signupForm.js'
 import './app/signinForm.js'
+import './app/logout.js'
+
+
+onAuthStateChanged(auth, async(user) =>{
+    console.log(user)
+    
+    loginCheck(user)
+    /*
+    if (user){
+        loginCheck(user)
+    } else {
+        loginCheck(user)
+    }
+    */
+    
+})
