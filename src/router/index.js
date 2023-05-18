0f70e220d9441d364e6162ec6815681f2aa1b18f
jona1100 <122585259+jona1100@users.noreply.github.com>
2023-05-18 02:04:44 -0400

    Matchmaking
    /matchmaking

--- src/router/index.js
+++ src/router/index.js
@@ -51,7 +51,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/PagTienda.vue')
-  }
+  },
+
+  {
+    path: '/matchmaking',
+    name: 'matchmaking',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/PagMatchmaking.vue')
+  },
 
 ]
 
